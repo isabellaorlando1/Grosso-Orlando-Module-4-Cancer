fb0477dcf44762529069085a475cb990f0ab04e5
isabellaorlando1 <isabellaorlando27@gmail.com>
2026-04-16 13:29:05 -0400

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,7 @@
+import pandas as pd # Pandas
+import numpy as np # Numpy
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+import seaborn as sns
+from sklearn.decomposition import PCA
